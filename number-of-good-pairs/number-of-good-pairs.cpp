535e8891ce6a13380b2fcfd901ae4ee5e4d65d25
Sanskriti Khare <60572554+sanskritikhare142@users.noreply.github.com>
2021-08-07 02:11:04 +0530

    Optimize number-of-good-pairs

--- number-of-good-pairs/number-of-good-pairs.cpp
+++ number-of-good-pairs/number-of-good-pairs.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int, int>m;
         int c=0;
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]==nums[j]){
-                    c++;
-                }
+        for(int i:nums){
+            if(!m.count(i)){
+                m[i]=1;
+            }
+            else{
+                int n=m.at(i);
+                c+=n;
+                m[i]=n+1;
             }
         }
         return c;
