dfa13cda6eb1faa5ab20faa1ba1f9db1720bf356
Sanskriti Khare <60572554+sanskritikhare142@users.noreply.github.com>
2021-08-06 23:59:39 +0530

    Time: 16 ms (99.36%), Space: 26 MB (30.73%) - LeetHub

---
+++ decode-xored-array/decode-xored-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int> ans(1, first);
+        for(int i=0;i<encoded.size();i++){
+            ans.push_back(ans[i]^encoded[i]);
+        }
+        return ans;
+    }
+};
