afc18414b9b4d8f95117a20801b9ccc20856308a
Sanskriti Khare <60572554+sanskritikhare142@users.noreply.github.com>
2021-08-06 23:00:40 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (34.86%) - LeetHub

---
+++ is-subsequence/is-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j=0;
+        for(int i=0;i<t.length();i++)
+        {
+            if(s[j]==t[i]){
+                j++;
+            }
+        }    
+        if(j<s.length()){
+            return false;
+        }
+        return true;
+    }
+};
