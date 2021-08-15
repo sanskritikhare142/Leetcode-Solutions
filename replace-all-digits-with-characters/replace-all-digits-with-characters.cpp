d5c8caa684b7e20471b7ef1428657eb00ee48f98
Sanskriti Khare <60572554+sanskritikhare142@users.noreply.github.com>
2021-08-15 23:25:36 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (45.44%) - LeetHub

--- replace-all-digits-with-characters/replace-all-digits-with-characters.cpp
+++ replace-all-digits-with-characters/replace-all-digits-with-characters.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
+    void shift(char *a, char *b){
+        *b=*a+*b-'0';
+    }
     string replaceDigits(string s) {
         for(int i=1;i<s.length();i+=2){
-            s[i]=s[i-1]+s[i]-'0';
+            shift(&s[i-1], &s[i]);
         }
         return s;
     }
