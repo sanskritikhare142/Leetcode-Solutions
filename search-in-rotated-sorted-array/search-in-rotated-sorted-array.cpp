87f1d3d29c6368eb7be6ef7d8b8cad086cb89d78
Sanskriti Khare <60572554+sanskritikhare142@users.noreply.github.com>
2021-08-18 22:07:03 +0530

    Time: 4 ms (72.67%), Space: 11.1 MB (28.57%) - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,44 +1,33 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& a, int i, int t, int l, int h, bool f){
+    int binarySearch(vector<int>& a, int i, int t, int l, int h){
         int m=(l+h)/2;
         if(l>h){
             return -1;
         }
         if(a[m]==t){
-            return f?(m+i>=a.size()-1?(m+i)%(a.size()-1):m+i+1):m;
+            return m;
         }
         else if(t<a[m]){
-            return binarySearch(a,i,t,l,m-1,f);
+            return binarySearch(a,i,t,l,m-1);
         }
         else{
-            return binarySearch(a, i,t,m+1,h,f);
+            return binarySearch(a, i,t,m+1,h);
         }
     }
     int search(vector<int>& a, int target) {
         int i, n=a.size();
-        bool f=0;
         if(n==1){
             if(a[0]==target) return 0;
             return -1;
         }
         for(i=0;i<n-1;i++){
             if(a[i+1]<a[i]){
-                f=1;
                 break;
             }
         }
-        vector<int> b;
-        for(int j=i+1;j<n;j++){
-            b.push_back(a[j]);
-        }
-        for(int j=i;j>=0;j--){
-            a[j+n-i-1]=a[j];
-        }
-        for(int j=0;j<n-i-1;j++){
-            a[j]=b[j];
-        }
-        // for(int )
-        return binarySearch(a, i, target, 0, n-1, f);
+        int x= binarySearch(a, i, target, 0, i);
+        int y= binarySearch(a, i, target, i+1, n-1);
+        return x==-1?(y==-1?-1:y):x;
     }
 };
